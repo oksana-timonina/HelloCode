3930f894d37be291748ab435df6e6fb613229bb7
Oksana Timonina <oks.timonina95@yandex.ru>
2022-10-23 13:55:26 +0300

    Add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Input your first number: ");
+int numberA = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Input your second number: ");
+int numberB = Int32.Parse(Console.ReadLine());
+int result = numberA + numberB;
+Console.WriteLine("Your Sum: ");
+Console.WriteLine(result);
